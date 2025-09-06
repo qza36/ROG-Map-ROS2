3bb8c8ae81fd6a9f2d2c94375e1f64ef577a8061
Zhiang QI <an9qi@yandex.com>
2025-09-06 16:11:07 +0800

    fix:修正配置文件路径

--- src/rog_map_ros/rogmapApp.cpp
+++ src/rog_map_ros/rogmapApp.cpp
@@ -5,7 +5,7 @@ int main(int argc, char** argv)
    rclcpp::init(argc, argv);
    rclcpp::NodeOptions options;
    auto node = std::make_shared<rclcpp::Node>("rog_map_node", options);
-   auto rog_map = std::make_shared<rog_map::ROGMapROS>(node, "/ros2_ws/src/cnn_perception/rog_map/config/config.yaml");
+   auto rog_map = std::make_shared<rog_map::ROGMapROS>(node, "/ros2_ws/src/COD_NAV_NEXT/cnn_perception/rog_map/config/config.yaml");
    rclcpp::spin(node);
    rclcpp::shutdown();
    return 0;
